97ced7c51178e53080d1a1032272ec4a7d450bc5
frostylegumes <pebblealan@gmail.com>
2021-03-17 15:35:08 -0400

    added testplot comp for simps and integral

--- testplot.py
+++ testplot.py
@@ -5,7 +5,7 @@ import math
 from scipy.integrate import simps
 
 t = np.arange(0, 10, 0.01) 
-y = np.sin(t)+1
+y = t
 plt.plot(t, y) 
 plt.title('matplotlib.pyplot.ginput() function Example', fontweight ="bold") 
 
@@ -18,18 +18,19 @@ plt.clf()
 endpoint1 = round(float(str(x[0]).split(', ')[0][1:]),2)
 endpoint2 = round(float(str(x[1]).split(', ')[0][1:]),2)
 
-print(endpoint1, endpoint2, t)
+print(endpoint1, endpoint2)
 
 t2 = np.arange(endpoint1, endpoint2, 0.01)
 
-print(t2)
+#print(t, t2)
 
 plt.close()
 
-y2 = np.sin(t2)+1
+y2 = t2
 plt.plot(t2, y2) 
 
 area = simps(y2, dx = 0.01)
 print(area)
+print((pow(endpoint2, 2)-pow(endpoint1, 2))/2)
 
 plt.show() 
